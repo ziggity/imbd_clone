73f81adfe84adb0acdcbaac5b23db68ee28d57b1
zach beecher <zachbeecher@gmail.com>
2017-10-27 23:08:26 -0700

    add users to plays model

---
+++ db/migrate/20171028060634_add_users_to_plays.rb
@@ -0,0 +1,5 @@
+class AddUsersToPlays < ActiveRecord::Migration[5.1]
+  def change
+    add_column :plays, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027051239) do
+ActiveRecord::Schema.define(version: 20171028060634) do
 
   create_table "plays", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20171027051239) do
     t.string "director"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
 end
